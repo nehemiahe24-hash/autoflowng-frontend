be9af12678a5471e4fb7be2e578763a72b56b06b
nehemiahe24-hash <nehemiahe24@gmail.com>
2026-05-30 19:45:23 +0100

    Delete main.tsx

--- main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Force dark class on html element for AUTOFLOWNG dark-only design
-document.documentElement.classList.add("dark");
-
-createRoot(document.getElementById("root")!).render(<App />);
